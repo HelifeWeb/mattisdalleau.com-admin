c1f51bdcfe45dbb3be97fa2aaf29355d4ceeb058
Mattis DALLEAU <mattis.dalleau@epitech.eu>
2024-09-12 23:27:04 +0400

    Update print_docker_registry_secrets.sh

--- print_docker_registry_secrets.sh
+++ print_docker_registry_secrets.sh
@@ -1,1 +1,1 @@
-cat /var/lib/hdci/registry-auth/watchtower/config.json | jq .auths.\"localhost:5000\".auth | sed 's/"//g' | base64 -d
+cat /var/lib/hdci/registry/auth/watchtower/config.json | jq .auths.\"localhost:5000\".auth | sed 's/"//g' | base64 -d
